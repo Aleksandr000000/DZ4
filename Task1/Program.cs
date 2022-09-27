115691cbc1fbd7423bd077698dc0a779b4bf112d
Александр <mrrotod@gmail.com>
2022-09-27 16:39:47 +0300

    дз к 4му семинару

---
+++ Task1/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задача 25: Напишите цикл, который принимает на вход два числа (A и B) 
+// и возводит число A в натуральную степень B.
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int GetNumberA()
+{
+    Console.Write("Введите  число: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+int GetNumberB()
+{
+    Console.Write("Введите  число: ");
+    return Convert.ToInt32(Console.ReadLine());
+}
+
+
+int Exponentiate (int A, int B)
+{
+    int result = 1;
+    for(int i=1; i <= B; i++)
+    {
+    result = result * A;
+    }
+    return result;
+}   
+int A = GetNumberA();
+int B = GetNumberB();
+Console.WriteLine($"Число {A} в натуральной степни {B} -> {Exponentiate ( A,  B )}"); 
+
+
+
---
+++ Task2/Program.cs
@@ -0,0 +1,35 @@
+﻿// Задача 27: Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.
+// 452 -> 11
+// 82 -> 10
+// 9012 -> 12
+
+int GetNumbr () /// метод ввода числа
+{
+    int  num = 0 ;
+    while (num < 10)
+    {
+        Console.WriteLine("Введите число  (которое больше 9) ");  
+        num = Convert.ToInt32(Console.ReadLine());
+    }   
+    return num;   
+}
+
+
+int Sumnumbers(int A )
+{
+    int summ = 0;
+    for ( int i = 3 ; i >= 0 ;  i--) 
+    {
+        summ += A/Convert.ToInt32(Math.Pow(10.01, i));
+        A = A%(int)Convert.ToInt32(Math.Pow(10.0,i));
+    }
+    return summ;
+
+}
+
+int A = GetNumbr ();
+int B = Sumnumbers(A);
+Console.WriteLine($"Сумма всех цифр в числе {A} --> {B}");
+
+
+
